4207df6418459e9210bd203bdf376aa6b9c6351f
John Constant <johnconstantis@gmail.com>
2022-07-31 17:36:35 +0000

    Add get player name function

--- run.py
+++ run.py
@@ -20,12 +20,27 @@ def welcome():
     print(" ")
     time.sleep(1)
 
-   
+
+def get_player_name():
+    """
+    Get player name
+    """
+    while True:
+        name = input("Please enter your name: ")
+        try:
+            name.isalpha()
+            print(f"Hello {name}, welcome to Hangman")
+            return name
+        except ValueError:
+            print('Only letters can be entered')
+
+
 def main():
     """
     Run program functions
     """
     welcome()
+    name = get_player_name()
 
 
 main()
