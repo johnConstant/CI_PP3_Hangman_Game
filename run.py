43f8f2d37d7c8627fbfe0c0e76e8e523c4963bfe
John Constant <johnconstantis@gmail.com>
2022-07-31 17:31:10 +0000

    Add welcome function

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Hangman game created for third project with Code Institute
+"""
+import time
+
+
+def welcome():
+    """
+    Show welcome screen
+    """
+    print("Welcome to:")
+    print(" ")
+    print("██   ██  █████  ███    ██  ██████  ███    ███  █████  ███    ██ ")
+    print("██   ██ ██   ██ ████   ██ ██       ████  ████ ██   ██ ████   ██ ")
+    print("███████ ███████ ██ ██  ██ ██   ███ ██ ████ ██ ███████ ██ ██  ██ ")
+    print("██   ██ ██   ██ ██  ██ ██ ██    ██ ██  ██  ██ ██   ██ ██  ██ ██ ")
+    print("██   ██ ██   ██ ██   ████  ██████  ██      ██ ██   ██ ██   ████ ")
+    print(" ")
+    print(" ")
+    print(" ")
+    time.sleep(1)
