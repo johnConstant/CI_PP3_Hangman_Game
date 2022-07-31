b3d3c7087b6a72c95bc28b48b96067575d07dfde
John Constant <johnconstantis@gmail.com>
2022-07-31 19:14:37 +0000

    Add get word function

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 Hangman game created for third project with Code Institute
 """
 import time
+from words import words
 
 
 def welcome():
@@ -53,12 +54,69 @@ def set_difficulty():
         raise ValueError('Please enter a valid difficulty level')
 
 
+def play_game(name, word):
+    """
+    Start game loop
+    """
+    # lives = 6
+    lives = set_difficulty()
+    guessed_letters = []
+    correct_guesses = []
+    word_list = list(word.strip(" "))
+    print('_ ' * len(word))
+    print(word)
+
+    while lives > 0:
+        guess = input('Please guess a letter: ').lower()
+        guessed_letters += guess
+
+        if guess in word:
+            print(Col.GREEN + f'Well done {name}! {guess} is in the word')
+            correct_guesses += guess
+            # https://www.youtube.com/watch?v=8ext9G7xspg
+            word_list = [letter if letter in guessed_letters
+                         else '_' for letter in word]
+            guessed_word = ''.join(word_list)
+            print(' ')
+            print(f'Current word: {guessed_word}')
+            print(' ')
+
+            if word == guessed_word:
+                print(Col.GREEN + f'Congratulations {name}, you guessed the correct word\
+, {word}')
+                print(' ')
+                print("Enter 'y' to play again and 'n' to end the game")
+                replay = input('Would you like to play another game: ').lower()
+                if replay == 'y':
+                    word = get_word(words)
+                    play_game(name, word)
+                else:
+                    break
+        else:
+            lives -= 1
+            print(graphic[lives])
+            print(Col.RED + f'Sorry but {guess} is not in the word.')
+    else:
+        print(' ')
+        print('Sorry but that is game over')
+        print(' ')
+        print("Enter 'y' to play again and 'n' to end the game")
+        replay = input('Would you like to play another game: ').lower()
+        if replay == 'y':
+            word = get_word(words)
+            play_game(name, word)
+        else:
+            exit()
+
+
 def main():
     """
     Run program functions
     """
     welcome()
     name = get_player_name()
+    word = get_word(words)
+    play_game(name, word)
 
 
 main()
