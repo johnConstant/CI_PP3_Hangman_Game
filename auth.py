5b30cffd0c40a05c53ca14f93d9ec4a8b753d6b9
John Constant <johnconstantis@gmail.com>
2022-08-01 10:23:09 +0000

    Set up Google APIs and confirm access to sheet

---
+++ auth.py
@@ -0,0 +1,21 @@
+'''Import gspread and Google OAuth modules'''
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('hangman_auth')
+
+details = SHEET.worksheet('auth_details')
+
+data = details.get_all_values()
+
+print(data)
